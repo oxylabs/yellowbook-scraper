db7088546c3ec737d82a62277c54a7837d331f9a
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:35:50 +0200

    Adding code examples/yellowbook_curl.sh

---
+++ code examples/yellowbook_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.yellowbook.com/lp/fa74ac/1/loading?fn=&ln=&city=united+states&state=all#."}'
