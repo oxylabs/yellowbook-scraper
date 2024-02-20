08b804df5831f2730612789f669ebc11001b38a4
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:35:48 +0200

    Adding code examples/yellowbook_php.php

---
+++ code examples/yellowbook_php.php
@@ -0,0 +1,27 @@
+<?php
+
+$params = [
+    'source' => 'universal_ecommerce',
+    'url' => 'https://www.yellowbook.com/lp/fa74ac/1/loading?fn=&ln=&city=united+states&state=all#.',
+];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://realtime.oxylabs.io/v1/queries");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERPWD, "user" . ":" . "pass1");
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
